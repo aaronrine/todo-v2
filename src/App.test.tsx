a090d8e63e1a67b2135700c9475e1b95dda7a140
Aaron Rine <aaronrine@gmail.com>
2021-03-16 13:24:26 -0700

    refactor(App.test): change it to test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,37 +3,37 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 
-describe('todo list', () => {
-  it.todo('is on the page')
-  it.todo('is empty on startup')
-  it.todo('stores and reads from local storage')
-  it.todo('has a todo description text input')
-  it.todo('has a todo priorty input')
-  it.todo('has an add button on the page')
-  it.todo('adds a valid todo to the list when add button is pressed')
-  it.todo('can reorder a todo item in front or behind of other todo items')
+describe('TodoList', () => {
+  test.todo('is on the page')
+  test.todo('is empty on startup')
+  test.todo('stores and reads from local storage')
+  test.todo('has a todo description text input')
+  test.todo('has a todo priorty input')
+  test.todo('has an add button on the page')
+  test.todo('adds a valid todo to the list when add button is pressed')
+  test.todo('can reorder a TodoItem in front or behind of other TodoItems')
 })
 
-describe('todo item', () => {
-  it.todo('is on the page')
-  it.todo('has description text on the page')
-  it.todo('has a priorty level on the page')
+describe('TodoItem', () => {
+  test.todo('is on the page')
+  test.todo('has description text on the page')
+  test.todo('has a priorty level on the page')
   describe('edit button', () => {
-    it.todo('has an edit button on the page')
-    it.todo('opens an edit form modal when edit button is pressed')
-    it.todo('has a todo description text input')
-    it.todo('has a todo priorty input')
-    it.todo('populates the edit form modal with the current todo data')
+    test.todo('has an edit button on the page')
+    test.todo('opens an edit form modal when edit button is pressed')
+    test.todo('has a todo description text input')
+    test.todo('has a todo priorty input')
+    test.todo('populates the edit form modal with the current todo data')
   })
   describe('delete button', () => {
-    it.todo('has a delete button on the page')
-    it.todo('is not in the todo list')
-    it.todo('is not on the page after delete button is pressed')
+    test.todo('has a delete button on the page')
+    test.todo('is not in the TodoList')
+    test.todo('is not on the page after delete button is pressed')
   })
   describe('mark button', () => {
-    it.todo('has a mark button when unmarked on the page')
-    it.todo('has a marked status on the page when marked button is pressed')
-    it.todo('has an unmark button when marked on the page')
-    it.todo('has an unmarked status on the page when unmark button is pressed')
+    test.todo('has a mark button when unmarked on the page')
+    test.todo('has a marked status on the page when marked button is pressed')
+    test.todo('has an unmark button when marked on the page')
+    test.todo('has an unmarked status on the page when unmark button is pressed')
   })
 })
