ac931558571705fa5123d172db99f2c6f97b2f95
Aaron Rine <aaronrine@gmail.com>
2021-03-16 13:14:22 -0700

    test(App.test): move setupTests to .

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,38 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('todo list', () => {
+  it.todo('is on the page')
+  it.todo('is empty on startup')
+  it.todo('stores and reads from local storage')
+  it.todo('has a todo description text input')
+  it.todo('has a todo priorty input')
+  it.todo('has an add button on the page')
+  it.todo('adds a valid todo to the list when add button is pressed')
+  it.todo('can reorder a todo item in front or behind of other todo items')
+})
+
+describe('todo item', () => {
+  it.todo('is on the page')
+  it.todo('has description text on the page')
+  it.todo('has a priorty level on the page')
+  describe('edit button', () => {
+    it.todo('has an edit button on the page')
+    it.todo('opens an edit form modal when edit button is pressed')
+    it.todo('has a todo description text input')
+    it.todo('has a todo priorty input')
+    it.todo('populates the edit form modal with the current todo data')
+  })
+  describe('delete button', () => {
+    it.todo('has a delete button on the page')
+    it.todo('is not in the todo list')
+    it.todo('is not on the page after delete button is pressed')
+  })
+  describe('mark button', () => {
+    it.todo('has a mark button when unmarked on the page')
+    it.todo('has a marked status on the page when marked button is pressed')
+    it.todo('has an unmark button when marked on the page')
+    it.todo('has an unmarked status on the page when unmark button is pressed')
+  })
+})
